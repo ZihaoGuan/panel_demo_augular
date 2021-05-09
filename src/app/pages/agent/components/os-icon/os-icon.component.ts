13e3533508bd832165b9ffd643eb3ac011aec201
zihaoguan <zgua779@aucklanduni.ac.nz>
2021-05-10 01:27:31 +1200

    add os icon

--- src/app/pages/agent/components/os-icon/os-icon.component.ts
+++ src/app/pages/agent/components/os-icon/os-icon.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-os-icon',
   templateUrl: './os-icon.component.html',
-  styleUrls: ['./os-icon.component.css']
+  styleUrls: ['./os-icon.component.sass']
 })
 export class OsIconComponent implements OnInit {
-
+  @Input() os: string;
+  source = '';
   constructor() { }
 
   ngOnInit(): void {
+    this.source = this.imageSource();
+  }
+
+  imageSource() {
+    switch (this.os) {
+      case "windows":
+        return "/assets/os-icons/windows.png";
+      case "ubuntu":
+        return "/assets/os-icons/ubuntu.png";
+      case "suse":
+        return "/assets/os-icons/suse.png";
+      case "debian":
+        return "/assets/os-icons/debin.png";
+      case "centos":
+        return "/assets/os-icons/cent_os.png";
+      default:
+        return "";
+    }
   }
 
 }
